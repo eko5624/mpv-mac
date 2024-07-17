1d2af995d2d75f26fd04f5fa2fa7385661338f1a
eko5624 <562433403@qq.com>
2024-07-17 23:24:43 +0800

    Update molten-vk.sh

--- scripts/molten-vk.sh
+++ scripts/molten-vk.sh
@@ -10,11 +10,14 @@ set -a; source build.env; source ver.sh; set +a
 #cd MoltenVK
 #./fetchDependencies --macos
 #make macos
-#sed -i '' "s|./libMoltenVK|$WORKSPACE/lib/libMoltenVK|g" Package/Latest/MoltenVK/dynamic/dylib/macOS/MoltenVK_icd.json
+#sed -i '' "s|./libMoltenVK|../../../Frameworks/libMoltenVK|g" Package/Latest/MoltenVK/dynamic/dylib/macOS/MoltenVK_icd.json
 #mkdir -p "$DIR/opt/share/vulkan/icd.d"
 #install -vm755 Package/Latest/MoltenVK/dynamic/dylib/macOS/libMoltenVK.dylib "$DIR/opt/lib"
 #install -vm644 Package/Latest/MoltenVK/dynamic/dylib/macOS/MoltenVK_icd.json "$DIR/opt/share/vulkan/icd.d"
 
+# Force 11 deployment target for all homebrew formulas
+find $(brew --repository)/Library/Taps -type f -iname *rb -exec sed -i "" $'s/def install/def install\\\n    ENV["MACOSX_DEPLOYMENT_TARGET"] = "11"\\\n/' {} \;
+          
 brew install molten-vk
 cd $DIR
 tar -zcvf molten-vk.tar.xz -C `brew --prefix molten-vk` .
