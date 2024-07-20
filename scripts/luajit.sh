c142241c43549cb028d03917092ec8f816bc98a2
eko5624 <562433403@qq.com>
2024-07-20 19:00:32 +0800

    -

--- scripts/luajit.sh
+++ scripts/luajit.sh
@@ -8,13 +8,11 @@ set -a; source build.env; source ver.sh; set +a
 cd $PACKAGES
 git clone https://github.com/LuaJIT/LuaJIT.git
 cd LuaJIT
-make -j $MJOBS amalg BUILDMODE=mixed PREFIX="$DIR/opt" XCFLAGS=-DLUAJIT_ENABLE_GC64 -DLUAJIT_UNWIND_INTERNAL
-make install PREFIX="$DIR/opt" XCFLAGS=-DLUAJIT_ENABLE_GC64 -DLUAJIT_UNWIND_INTERNAL
+make -j $MJOBS amalg PREFIX="$DIR/opt" XCFLAGS=-DLUAJIT_ENABLE_GC64
+make install PREFIX="$DIR/opt" XCFLAGS=-DLUAJIT_ENABLE_GC64
 
-rm $DIR/opt/lib/*.dylib
-rm -rf lua
 sed -i "" 's/opt/workspace/g' $DIR/opt/lib/pkgconfig/*.pc
 sed -i "" 's/Libs.private: -Wl,-E -lm -ldl/Libs.private: -lm -ldl/g' $DIR/opt/lib/pkgconfig/*.pc
 
 cd $DIR
-tar -zcvf luajit2.tar.xz -C $DIR/opt .
+tar -zcvf luajit.tar.xz -C $DIR/opt .
---
+++ scripts/vapoursynth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")" && cd ..
+set -a; source build.env; source ver.sh; set +a
+
+# Video processing framework with simplicity in mind
+cd $PACKAGES
+git clone https://github.com/vapoursynth/vapoursynth.git --branch R$VER_VAPOURSYNTH
+cd vapoursynth
+./autogen.sh
+./configure \
+	--prefix="$DIR/opt" \
+  --disable-silent-rules \
+  --disable-dependency-tracking \
+  --with-cython="$WORKSPACE/bin/python" \
+  --with-plugindir="$DIR/opt/lib/vapoursynth" \
+  --with-python_prefix="$DIR/opt" \
+  --with-python_exec_prefix="$DIR/opt"
+make -j $MJOBS
+make install
+
+sed -i "" 's/opt/workspace/g' $DIR/opt/lib/pkgconfig/*.pc
+
+cd $DIR
+tar -zcvf vapoursynth.tar.xz -C $DIR/opt .
