3b99813ef12662ef842cd0568d1e7fceeb096c7b
eko5624 <562433403@qq.com>
2024-07-24 18:54:46 +0800

    -

--- scripts/libdovi.sh
+++ scripts/libdovi.sh
@@ -5,7 +5,6 @@ cd "$(dirname "$0")" && cd ..
 set -a; source build.env; source ver.sh; set +a
 
 # Library to read and write Dolby Vision metadata (C-API)
-cd $PACKAGES
 if [ ! -d "$WORKSPACE/rust/.cargo" ]; then
   export RUSTUP_HOME="${WORKSPACE}"/rust/.rustup
   export CARGO_HOME="${WORKSPACE}"/rust/.cargo
@@ -24,6 +23,9 @@ fi
 if [ ! -d "$WORKSPACE/rust/.rustup" ]; then
   $WORKSPACE/rust/.cargo/bin/rustup default stable-$ARCH-apple-darwin
 fi
+
+PATH="$WORKSPACE/rust/.rustup/toolchains/stable-$ARCH-apple-darwin/bin:$PATH"
+cd $PACKAGES
 git clone https://github.com/quietvoid/dovi_tool.git
 cd dovi_tool/dolby_vision
 mkdir build
