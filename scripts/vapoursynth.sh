1cdfe63bf9e820072d80f709e83c08d586530df4
eko5624 <562433403@qq.com>
2024-07-30 21:05:48 +0800

    -

--- scripts/vapoursynth.sh
+++ scripts/vapoursynth.sh
@@ -11,15 +11,10 @@ cd $PACKAGES
 git clone https://github.com/vapoursynth/vapoursynth.git --branch R$VER_VAPOURSYNTH
 cd vapoursynth
 ./autogen.sh
-sed -i "" 's|pkglibdir = $(libdir)|pkglibdir = $(exec_prefix)|g' Makefile.in
 ./configure \
   --prefix="$DIR/opt" \
   --disable-silent-rules \
-  --disable-dependency-tracking \
-  --with-cython="$WORKSPACE/bin/cython" \
-  --with-plugindir="$DIR/opt/lib/vapoursynth" \
-  --with-python_prefix="$DIR/opt" \
-  --with-python_exec_prefix="$DIR/opt"
+  --disable-dependency-tracking
 make -j $MJOBS
 make install
 sed -i "" 's/opt/workspace/g' $DIR/opt/lib/pkgconfig/*.pc
