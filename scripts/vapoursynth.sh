3eca50b2c0e06d59e0868b059083cbcabc698953
eko5624 <562433403@qq.com>
2024-07-31 22:09:53 +0800

    vapoursynth

--- scripts/vapoursynth.sh
+++ scripts/vapoursynth.sh
@@ -6,7 +6,7 @@ set -a; source build.env; source ver.sh; set +a
 
 # Video processing framework with simplicity in mind
 # depends on: zimg
-pip install -U cython wheel
+PKG_CONFIG="pkg-config"
 cd $PACKAGES
 git clone https://github.com/vapoursynth/vapoursynth.git --branch R$VER_VAPOURSYNTH
 cd vapoursynth
