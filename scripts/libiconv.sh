30f468d97fbc990eecd3b6fda885d3736a9f8f84
eko5624 <562433403@qq.com>
2024-08-04 21:37:03 +0800

    Update libiconv.sh

--- scripts/libiconv.sh
+++ scripts/libiconv.sh
@@ -23,6 +23,7 @@ make install
 
 mkdir -p $DIR/opt/lib/pkgconfig
 sed -i "" 's|/Users/runner/work/mpv-mac/mpv-mac/workspace|\${DIR}/opt|g' $DIR/iconv.pc
+sed -i "" 's|@VERSION@|'"${VER_LIBICONV}"'|g' $DIR/iconv.pc
 cp $DIR/iconv.pc $DIR/opt/lib/pkgconfig
 cat $DIR/opt/lib/pkgconfig/iconv.pc
 
