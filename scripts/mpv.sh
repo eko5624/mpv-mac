2a1b1fc3ded01c552ae22b9e1f3403f18d40c443
eko5624 <562433403@qq.com>
2024-11-11 09:47:53 +0800

    Update mpv.sh

--- scripts/mpv.sh
+++ scripts/mpv.sh
@@ -6,10 +6,10 @@ set -a; source build.env; source ver.sh; set +a
 
 cd $PACKAGES
 git clone https://github.com/mpv-player/mpv.git
+cd mpv
 git reset --hard 23843b4aa594dc8c885575f3d237cde3c29398a2
 
 LDFLAGS+=" -Wl,-no_compact_unwind"
-cd mpv
 #export TOOLCHAINS=$(/usr/libexec/PlistBuddy -c "Print :CFBundleIdentifier" /Library/Developer/Toolchains/swift-latest.xctoolchain/Info.plist)
 meson setup build \
   --buildtype=release \
