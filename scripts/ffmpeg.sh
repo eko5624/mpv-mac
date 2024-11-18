471452b157d92419075ae547d5035a11f1510750
eko5624 <562433403@qq.com>
2024-11-18 16:21:16 +0800

    Update ffmpeg.sh

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -19,6 +19,10 @@ patch -p1 -i 1.patch
 curl -OL https://raw.githubusercontent.com/wiki/fraunhoferhhi/vvdec/data/patch/v6-0001-avcodec-add-external-dec-libvvdec-for-H266-VVC.patch
 patch -p1 -i v6-0001-avcodec-add-external-dec-libvvdec-for-H266-VVC.patch
 
+# support hevc flv
+curl -OL https://github.com/yt-dlp/FFmpeg-Builds/blob/master/patches/ffmpeg/master/0001-Nonstandard-HEVC-over-FLV.patch
+patch -p1 -i 0001-Nonstandard-HEVC-over-FLV.patch
+
 ./configure \
   --prefix="$DIR/opt" \
   --pkg-config-flags=--static \
