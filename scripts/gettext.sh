89ff1222331901440c0d958499a1e07e2013eb8b
eko5624 <562433403@qq.com>
2024-12-03 22:01:08 +0800

    -

---
+++ scripts/gettext.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")" && cd ..
+set -a; source build.env; source ver.sh; set +a
+
+# gettext: GNU internationalization (i18n) and localization (l10n) library
+# depends on: libxml2(zlib), ncurses
+cd $PACKAGES
+curl -OL "https://ftpmirror.gnu.org/gettext/gettext-$VER_GETTEXT.tar.gz"
+tar -xvf gettext-$VER_GETTEXT.tar.gz 2>/dev/null >/dev/null
+cd gettext-$VER_GETTEXT
+./configure \
+  --prefix="$DIR/opt"\
+  --enable-static \
+  --disable-shared \
+  --disable-silent-rules \
+  --with-libiconv-prefix="${WORKSPACE}" \
+  --with-included-gettext \
+  --with-included-glib \
+  --with-included-libcroco \
+  --with-included-libunistring \
+  --with-included-libxml \
+  --disable-java \
+  --disable-csharp \
+  --without-git \
+  --without-cvs \
+  --without-xz
+make -j $MJOBS
+make install
+cp $DIR/intl.pc ${WORKSPACE}/lib/pkgconfig
+sed -i "" 's|@prefix@|'"${WORKSPACE}"'|g' $DIR/opt/lib/pkgconfig/intl.pc
+sed -i "" 's|@VERSION@|'"${VER_GETTEXT}"'|g' $DIR/opt/lib/pkgconfig/intl.pc
+
+cd $DIR
+tar -zcvf gettext.tar.xz -C $DIR/opt .
