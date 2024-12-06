981d684a9dc16688fb12fab49715e927a1251d81
eko5624 <562433403@qq.com>
2024-12-06 09:50:20 +0800

    Update libvpx.sh

--- scripts/libvpx.sh
+++ scripts/libvpx.sh
@@ -13,7 +13,7 @@ sed "s/,--version-script//g" build/make/Makefile >build/make/Makefile.patched
 sed "s/-Wl,--no-undefined -Wl,-soname/-Wl,-undefined,error -Wl,-install_name/g" build/make/Makefile.patched >build/make/Makefile
 cd build
 ../configure \
-  --host=x86_64-apple-darwin \
+  --target=x86_64-darwin20-gcc \
   --prefix="$DIR/opt" \
   --disable-dependency-tracking \
   --disable-examples \
