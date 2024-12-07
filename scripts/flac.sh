8f71c583a1ce170c997989f8707ebc74925fa789
eko5624 <562433403@qq.com>
2024-12-07 21:11:18 +0800

    Update flac.sh

--- scripts/flac.sh
+++ scripts/flac.sh
@@ -9,22 +9,19 @@ set -a; source build.env; source ver.sh; set +a
 cd $PACKAGES
 git clone https://gitlab.xiph.org/xiph/flac.git
 cd flac
-mkdir out && cd out
-cmake .. \
-  -G "Ninja" \
-  -DCMAKE_INSTALL_PREFIX="$DIR/opt" \
-  -DCMAKE_TOOLCHAIN_FILE="$DIR/cmake_$ARCHS.txt" \
-  -DCMAKE_OSX_ARCHITECTURES=x86_64 \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=11 \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_NAME_DIR="$DIR/opt/lib" \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DBUILD_DOCS=OFF \
-  -DBUILD_EXAMPLES=OFF \
-  -DBUILD_TESTING=OFF \
-  -DINSTALL_MANPAGES=OFF
-cmake --build . -j $MJOBS
-cmake --install .
+./autogen.sh
+./configure \
+  --host=x86_64-apple-darwin \
+  --prefix="$DIR/opt" \
+  --enable-static \
+  --disable-shared \
+  --disable-doxygen-docs \
+  --disable-xmms-plugin \
+  --disable-thorough-tests \
+  --disable-oggtest \
+  --disable-examples
+make -j $MJOBS
+make install
 
 sed -i "" 's/opt/workspace/g' $DIR/opt/lib/pkgconfig/*.pc
 
