eaef32c4251f2ed59b9a515d8830ca045e3674b4
eko5624 <562433403@qq.com>
2024-12-08 17:11:00 +0800

    Update zlib.sh

--- scripts/zlib.sh
+++ scripts/zlib.sh
@@ -9,23 +9,12 @@ cd $PACKAGES
 curl -OL "https://github.com/madler/zlib/releases/download/v$VER_ZLIB/zlib-$VER_ZLIB.tar.xz"
 tar -xvf zlib-$VER_ZLIB.tar.xz 2>/dev/null >/dev/null
 cd zlib-$VER_ZLIB
-mkdir out && cd out
-cmake .. \
-  -G "Ninja" \
-  -DCMAKE_INSTALL_PREFIX="$DIR/opt" \
-  -DCMAKE_TOOLCHAIN_FILE="$DIR/cmake_$ARCHS.txt" \
-  -DCMAKE_OSX_ARCHITECTURES=x86_64 \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=11 \
-  -DCMAKE_INSTALL_NAME_DIR="$DIR/opt/lib" \
-  -DINSTALL_PKGCONFIG_DIR="$DIR/opt/lib/pkgconfig" \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DSKIP_INSTALL_LIBRARIES=OFF \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DZLIB_COMPAT=ON \
-  -DZLIB_ENABLE_TESTS=OFF \
-  -DZLIBNG_ENABLE_TESTS=OFF
-cmake --build .
-cmake --install .
+./configure \
+  --archs="-arch x86_64" \
+  --prefix="$DIR/opt" \
+  --static
+make -j $MJOBS
+make install
 
 sed -i "" 's/opt/workspace/g' $DIR/opt/lib/pkgconfig/*.pc
 
