6d6ff0abc655f5a6a0dc139353c70d3064295fb2
eko5624 <562433403@qq.com>
2024-12-09 16:50:46 +0800

    -

--- scripts/libxml2.sh
+++ scripts/libxml2.sh
@@ -9,24 +9,6 @@ set -a; source build.env; source ver.sh; set +a
 cd $PACKAGES
 git clone https://github.com/GNOME/libxml2.git
 cd libxml2
-# Fix crash when using Python 3 using Fedora's patch.
-# Reported upstream:
-# https://bugzilla.gnome.org/show_bug.cgi?id=789714
-# https://gitlab.gnome.org/GNOME/libxml2/issues/12
-#execute curl $CURL_RETRIES -L --silent -o fix_crash.patch "https://bugzilla.opensuse.org/attachment.cgi?id=746044"
-#execute patch -p1 -i fix_crash.patch
-autoreconf -fvi
-./configure \
-  --host=x86_64-apple-darwin \
-  --prefix="$DIR/opt" \
-  --without-python \
-  --without-lzma \
-  --with-iconv="${WORKSPACE}" \
-  --disable-shared \
-  --enable-static
-make -j $MJOBS
-make install
-
 mkdir out && cd out
 cmake .. \
   -G "Ninja" \
