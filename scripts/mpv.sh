37cef6066ebafc3c4ea1a1c898314e9fb0a1bcc8
eko5624 <562433403@qq.com>
2024-12-14 13:02:56 +0800

    Update mpv.sh

--- scripts/mpv.sh
+++ scripts/mpv.sh
@@ -9,18 +9,17 @@ git clone https://github.com/mpv-player/mpv.git
 cd mpv
 #git reset --hard 23843b4aa594dc8c885575f3d237cde3c29398a2
 #export TOOLCHAINS=$(/usr/libexec/PlistBuddy -c "Print :CFBundleIdentifier" /Library/Developer/Toolchains/swift-latest.xctoolchain/Info.plist)
-CFLAGS+=" -I$WORKSPACE/include/libplacebo"
-CFLAGS+=" -I$WORKSPACE/include/libavcodec"
-CFLAGS+=" -I$WORKSPACE/include/libavdevice"
-CFLAGS+=" -I$WORKSPACE/include/libavfilter"
-CFLAGS+=" -I$WORKSPACE/include/libavformat"
-CFLAGS+=" -I$WORKSPACE/include/libavutil"
-CFLAGS+=" -I$WORKSPACE/include/libpostproc"
-CFLAGS+=" -I$WORKSPACE/include/libswresample"
-CFLAGS+=" -I$WORKSPACE/include/libswscale"
-CFLAGS+=" -I$WORKSPACE/include/vulkan"
-CFLAGS+=" -I$WORKSPACE/include/vk_video"
-CPPFLAGS="$CFLAGS"
+ln -s $WORKSPACE/include/libplacebo libplacebo
+ln -s $WORKSPACE/include/libavcodec libavcodec
+ln -s $WORKSPACE/include/libavdevice libavdevice
+ln -s $WORKSPACE/include/libavfilter libavfilter
+ln -s $WORKSPACE/include/libavformat libavformat
+ln -s $WORKSPACE/include/libavutil libavutil
+ln -s $WORKSPACE/include/libpostproc libpostproc
+ln -s $WORKSPACE/include/libswresample libswresample
+ln -s $WORKSPACE/include/libswscale libswscale
+ln -s $WORKSPACE/include/vulkan vulkan
+ln -s $WORKSPACE/include/vk_video vk_video
 meson setup build \
   --buildtype=release \
   -Dwrap_mode=nodownload \
