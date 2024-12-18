7bd5d915b89cd6a4431da9c17f5c20db3c1963c8
eko5624 <562433403@qq.com>
2024-12-18 21:23:14 +0800

    Update libdovi.sh

--- scripts/libdovi.sh
+++ scripts/libdovi.sh
@@ -7,19 +7,22 @@ set -a; source build.env; source ver.sh; set +a
 # Library to read and write Dolby Vision metadata (C-API)
 if [ "$ARCHS" == "x86_64" ]; then
   rustup target add x86_64-apple-darwin
-  #cargo install cargo-c
+  cargo install cargo-c
 elif [ "$ARCHS" == "arm64" ]; then
   rustup target add aarch64-apple-darwin
-  #cargo install cargo-c
+  cargo install cargo-c
 fi
 
 cd $PACKAGES
 git clone https://github.com/quietvoid/dovi_tool.git
-cd dovi_tool
-cargo build --release \
+cd dovi_tool/dolby_vision
+mkdir build
+cargo cinstall \
   --manifest-path=Cargo.toml \
-  --target-dir="$DIR/opt" \
-  --target=$ARCH-apple-darwin
+  --prefix="$DIR/opt" \
+  --target=$ARCH-apple-darwin \
+  --release \
+  --library-type=staticlib
 
 #sed -i "" 's/opt/workspace/g' $DIR/opt/lib/pkgconfig/*.pc
 
