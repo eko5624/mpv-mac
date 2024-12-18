ed923a22688d3d8af03d5f9208d09b88f43281ad
eko5624 <562433403@qq.com>
2024-12-18 20:10:06 +0800

    Update libdovi.sh

--- scripts/libdovi.sh
+++ scripts/libdovi.sh
@@ -19,9 +19,8 @@ cd dovi_tool/dolby_vision
 mkdir build
 export CARGO_BUILD_TARGET_DIR=build
 export CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
-cargo cinstall \
+cargo build \
   --manifest-path=Cargo.toml \
-  --prefix="$DIR/opt" \
   --target=$ARCH-apple-darwin \
   --release \
   --library-type=staticlib
