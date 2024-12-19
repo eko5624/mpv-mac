cb3a3eb5726ab7a3a3ad70c456b1dc8f02062769
eko5624 <562433403@qq.com>
2024-12-19 21:38:39 +0800

    Update libdovi.sh

--- scripts/libdovi.sh
+++ scripts/libdovi.sh
@@ -5,15 +5,22 @@ cd "$(dirname "$0")" && cd ..
 set -a; source build.env; source ver.sh; set +a
 
 # Library to read and write Dolby Vision metadata (C-API)
-if [ ! -d "$TOOLS/rust/.cargo" ]; then
   export RUSTUP_HOME="${TOOLS}/rust/.rustup"
   export CARGO_HOME="${TOOLS}/rust/.cargo"
   if [ "$(uname -m)" == "x86_64" ]; then
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target x86_64-apple-darwin --no-modify-path
-    curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-x86_64.zip
-    7z x cargo-c-macos-x86_64.zip
-    cp cargo-bin/*  $RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin
-    PATH="$RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin:$PATH"
+    if [ "$ARCHS" == "x86_64" ]; then
+      curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target x86_64-apple-darwin --no-modify-path
+      curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-x86_64.zip
+      7z x cargo-c-macos-x86_64.zip
+      cp cargo-bin/*  $RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin
+      PATH="$RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin:$PATH"
+    elif [ "$ARCHS" == "arm64" ]; then
+      curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target aarch64-apple-darwin --no-modify-path
+      curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-x86_64.zip
+      7z x cargo-c-macos-x86_64.zip
+      cp cargo-bin/*  $RUSTUP_HOME/toolchains/stable-aarch64-apple-darwin/bin
+      PATH="$RUSTUP_HOME/toolchains/stable-aarch64-apple-darwin/bin:$PATH"
+    fi
   elif [ "$(uname -m)" == "arm64" ]; then
     curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target aarch64-apple-darwin --no-modify-path
     curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-arm64.zip
@@ -21,7 +28,6 @@ if [ ! -d "$TOOLS/rust/.cargo" ]; then
     cp cargo-bin/*  $RUSTUP_HOME/toolchains/stable-aarch64-apple-darwin/bin
     PATH="$RUSTUP_HOME/toolchains/stable-aarch64-apple-darwin/bin:$PATH"
   fi
-fi
 
 cd $PACKAGES
 git clone https://github.com/quietvoid/dovi_tool.git
