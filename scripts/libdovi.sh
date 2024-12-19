d50c9122dfa2b09aa31a6adf0d289dc5310d4982
eko5624 <562433403@qq.com>
2024-12-19 16:11:22 +0800

    Update libdovi.sh

--- scripts/libdovi.sh
+++ scripts/libdovi.sh
@@ -7,7 +7,9 @@ set -a; source build.env; source ver.sh; set +a
 # Library to read and write Dolby Vision metadata (C-API)
 if [ "$ARCHS" == "x86_64" ]; then
   rustup target add x86_64-apple-darwin
-  cargo install cargo-c
+  curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-x86_64.zip
+  7z x cargo-c-macos-x86_64.zip
+  cp cargo-bin/* $RUSTUP_HOME/toolchains/stable-$ARCH-apple-darwin/bin
 elif [ "$ARCHS" == "arm64" ]; then
   #rustup target add x86_64-apple-darwin
   curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-x86_64.zip
