6fc3a63c3e816a8e5137fcc3206d6bbc6df1b09d
eko5624 <562433403@qq.com>
2024-12-19 18:10:27 +0800

    Update libdovi.sh

--- scripts/libdovi.sh
+++ scripts/libdovi.sh
@@ -24,7 +24,7 @@ if [ ! -d "$TOOLS/rust/.cargo" ]; then
   fi
 fi
 
-PATH="$RUSTUP_HOME/toolchains/stable-$ARCH-apple-darwin/bin:$PATH"
+PATH="$RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin:$PATH"
 cd $PACKAGES
 git clone https://github.com/quietvoid/dovi_tool.git
 cd dovi_tool/dolby_vision
