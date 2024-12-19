53e05bb7cc17dc34532aab6841bf65f5449566a7
eko5624 <562433403@qq.com>
2024-12-19 21:42:54 +0800

    Update libdovi.sh

--- scripts/libdovi.sh
+++ scripts/libdovi.sh
@@ -5,22 +5,20 @@ cd "$(dirname "$0")" && cd ..
 set -a; source build.env; source ver.sh; set +a
 
 # Library to read and write Dolby Vision metadata (C-API)
-if [ ! -d "$TOOLS/rust/.cargo" ]; then
-  export RUSTUP_HOME="${TOOLS}/rust/.rustup"
-  export CARGO_HOME="${TOOLS}/rust/.cargo"
-  if [ "$(uname -m)" == "x86_64" ]; then
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target x86_64-apple-darwin --no-modify-path
-    curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-x86_64.zip
-    7z x cargo-c-macos-x86_64.zip
-    cp cargo-bin/*  $RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin
-    PATH="$RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin:$PATH"
-  elif [ "$(uname -m)" == "arm64" ]; then
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target aarch64-apple-darwin --no-modify-path
-    curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-arm64.zip
-    7z x cargo-c-macos-arm64.zip
-    cp cargo-bin/*  $RUSTUP_HOME/toolchains/stable-aarch64-apple-darwin/bin
-    PATH="$RUSTUP_HOME/toolchains/aarch64-x86_64-apple-darwin/bin:$PATH"
-  fi
+export RUSTUP_HOME="${TOOLS}/rust/.rustup"
+export CARGO_HOME="${TOOLS}/rust/.cargo"
+if [ "$(uname -m)" == "x86_64" ]; then
+  curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target x86_64-apple-darwin --no-modify-path
+  curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-x86_64.zip
+  7z x cargo-c-macos-x86_64.zip
+  cp cargo-bin/*  $RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin
+  PATH="$RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin:$PATH"
+elif [ "$(uname -m)" == "arm64" ]; then
+  curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target aarch64-apple-darwin --no-modify-path
+  curl -OL https://github.com/eko5624/mpv-mac/releases/download/tools/cargo-c-macos-arm64.zip
+  7z x cargo-c-macos-arm64.zip
+  cp cargo-bin/*  $RUSTUP_HOME/toolchains/stable-aarch64-apple-darwin/bin
+  PATH="$RUSTUP_HOME/toolchains/aarch64-x86_64-apple-darwin/bin:$PATH"
 fi
 
 PATH="$RUSTUP_HOME/toolchains/stable-x86_64-apple-darwin/bin:$PATH"
