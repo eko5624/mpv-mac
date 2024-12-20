26ff94d2394f5ec073ab2864e2241875a96d54e9
eko5624 <562433403@qq.com>
2024-12-20 21:01:50 +0800

    Update luajit.sh

--- scripts/luajit.sh
+++ scripts/luajit.sh
@@ -11,14 +11,12 @@ cd LuaJIT
 make -C src \
   DEFAULT_CC=clang \
   CROSS="/Applications/Xcode_15.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/" \
-  TARGET_FLAGS="-arch $ARCHS -isysroot $SDKROOT" \
   TARGET_SYS=Darwin \
   PREFIX="$DIR/opt" \
   amalg
 make \
   DEFAULT_CC=clang \
   CROSS="/Applications/Xcode_15.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/" \
-  TARGET_FLAGS="-arch $ARCHS -isysroot $SDKROOT" \
   TARGET_SYS=Darwin \
   PREFIX="$DIR/opt" \
   install
