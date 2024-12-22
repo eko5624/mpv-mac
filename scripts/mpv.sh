e91f2f5a1e4afd3ab324ded1cb3c64366a1a000a
eko5624 <562433403@qq.com>
2024-12-22 22:22:17 +0800

    Update mpv.sh

--- scripts/mpv.sh
+++ scripts/mpv.sh
@@ -9,7 +9,7 @@ cd $PACKAGES
 git clone https://github.com/mpv-player/mpv.git
 cd mpv
 export CFLAGS="$CFLAGS -Wno-error=deprecated -Wno-error=deprecated-declarations -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3"
-export LDFLAGS+=" -Wl,-no_compact_unwind"
+export LDFLAGS="$LDFLAGS -Wl,-no_compact_unwind"
 #git reset --hard 23843b4aa594dc8c885575f3d237cde3c29398a2
 #export TOOLCHAINS=$(/usr/libexec/PlistBuddy -c "Print :CFBundleIdentifier" /Library/Developer/Toolchains/swift-latest.xctoolchain/Info.plist)
 meson setup build \
