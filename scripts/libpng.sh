21472f0376a7b423e3c10bacd4ea4efaa8242c3a
eko5624 <562433403@qq.com>
2025-10-25 22:23:16 +0800

    libpng: switch to cmake build

--- scripts/libpng.sh
+++ scripts/libpng.sh
@@ -5,22 +5,27 @@ cd "$(dirname "$0")" && cd ..
 set -a; source build.env; source ver.sh; set +a
 
 myconf=(
-    --prefix="$DIR/opt"
-    --disable-shared
-    --enable-static
-)
+    -DCMAKE_INSTALL_PREFIX="$DIR/opt"
+    -DCMAKE_OSX_ARCHITECTURES=$ARCHS
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_TARGET
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_NAME_DIR="$DIR/opt/lib"
+    -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON
+    -DSKIP_INSTALL_EXPORT=ON
+    -DPNG_SHARED=OFF
+    -DPNG_FRAMEWORK=OFF
+    -DPNG_TESTS=OFF
+    -DPNG_TOOLS=OFF
 
 if [[ ("$(uname -m)" == "x86_64") && ("$ARCHS" == "arm64") ]]; then
     myconf+=(
-        --host=aarch64-apple-darwin
-        --target=arm64-apple-macos11.0
+        -DCMAKE_TOOLCHAIN_FILE=$DIR/cmake_arm64.txt
     )
 fi
 
 if [[ ("$(uname -m)" == "arm64") && ("$ARCHS" == "x86_64") ]]; then
     myconf+=(
-        --host=x86_64-apple-darwin
-        --target=x86_64-apple-macos11.0
+        -DCMAKE_TOOLCHAIN_FILE=$DIR/cmake_x86_64.txt
     )
 fi
 
@@ -29,9 +34,10 @@ fi
 cd $PACKAGES
 git clone https://github.com/glennrp/libpng.git
 cd libpng
-./configure "${myconf[@]}"
-make -j $MJOBS
-make install
+mkdir out && cd out
+cmake .. -G "Ninja" "${myconf[@]}"
+cmake --build . -j $MJOBS
+cmake --install .
 
 sed -i "" 's/opt/workspace/g' $DIR/opt/lib/pkgconfig/libpng16.pc
 
