01c27808e13357348bbd29bf7065b892b31d73a4
eko5624 <562433403@qq.com>
2025-11-06 15:12:55 +0800

    libbluray: fix build

--- scripts/libbluray.sh
+++ scripts/libbluray.sh
@@ -5,15 +5,14 @@ cd "$(dirname "$0")" && cd ..
 set -a; source build.env; source ver.sh; set +a
 
 myconf=(
-    --prefix="$DIR/opt" \
-    --disable-dependency-tracking \
-    --disable-silent-rules \
-    --disable-bdjava-jar \
-    --disable-examples \
-    --without-fontconfig \
-    --without-libxml2 \
-    --disable-shared \
-    --enable-static
+    --prefix="$DIR/opt"
+    --buildtype=release
+    --libdir="$DIR/opt/lib"
+    --default-library=static
+    -Dbdj_jar=disabled
+    -Dfontconfig=enabled
+    -Dfreetype=enabled
+    -Dlibxml2=enabled
 )
 
 if [[ ("$(uname -m)" == "x86_64") && ("$ARCHS" == "arm64") ]]; then
@@ -34,12 +33,12 @@ fi
 # depends on: fontconfig(expat, bzip2, freetype2, gettext(libxml2 ncurses)), freetype2(libpng(zlib)), libxml2(ncurses)
 rm $WORKSPACE/lib/*.la
 cd $PACKAGES
-curl -OL  "https://download.videolan.org/videolan/libbluray/$VER_LIBBLURAY/libbluray-$VER_LIBBLURAY.tar.bz2"
-tar -xvf libbluray-$VER_LIBBLURAY.tar.bz2 2>/dev/null >/dev/null
+curl -OL  "https://download.videolan.org/videolan/libbluray/1.4.0/libbluray-$VER_LIBBLURAY.tar.xz"
+tar -xvf libbluray-$VER_LIBBLURAY.tar.xz 2>/dev/null >/dev/null
 cd libbluray-$VER_LIBBLURAY
-./configure "${myconf[@]}"
-make -j $MJOBS
-make install
+meson setup work "${myconf[@]}"
+meson compile -C work
+meson install -C work
 
 sed -i "" 's|opt|workspace|g' $DIR/opt/lib/pkgconfig/libbluray.pc
 sed -i "" 's|/Users/runner/work/mpv-mac/mpv-mac/packages ||g' $DIR/opt/lib/pkgconfig/libbluray.pc
