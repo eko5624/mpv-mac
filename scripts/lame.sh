9d1860b775800e5f15ad15ee3d93d8fdb1468006
eko5624 <562433403@qq.com>
2025-11-14 21:04:40 +0800

    lame: migrate to my folk repo

--- scripts/lame.sh
+++ scripts/lame.sh
@@ -29,9 +29,8 @@ fi
 # High quality MPEG Audio Layer III (MP3) encoder
 # depends on: ncurses
 cd $PACKAGES
-curl -OL "http://downloads.sourceforge.net/lame/lame-$VER_LAME.tar.gz"
-tar -xvf lame-$VER_LAME.tar.gz 2>/dev/null >/dev/null
-cd lame-$VER_LAME
+git clone https://github.com/eko5624/lame.git
+cd lame
 # Fix undefined symbol error _lame_init_old
 # https://sourceforge.net/p/lame/mailman/message/36081038/
 sed -i "" '/lame_init_old/d' include/libmp3lame.sym
